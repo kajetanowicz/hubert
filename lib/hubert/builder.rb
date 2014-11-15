91ded40a7f8aab497169e9512a9d4e4f4bdd254d
Andrzej Kajetanowicz <andrzej.kajetanowicz@gmail.com>
2014-11-15 13:19:31 +0000

    Allow builder to assingn a protocol

--- lib/hubert/builder.rb
+++ lib/hubert/builder.rb
@@ -1,12 +1,31 @@
 require 'uri'
 
 module Hubert
+
+  InvalidProtocol = Class.new(StandardError)
+
   class Builder
 
-    attr_reader :host
+    attr_reader :protocol, :host
 
     def initialize
       yield(self) if block_given?
+    end
+
+    def protocol=(protocol)
+      if protocol =~ /(http)|(HTTP)/i
+        @protocol = protocol.downcase
+      else
+        fail InvalidProtocol, "Provided protocol: [#{protocol}] is invalid"
+      end
+    end
+
+    def http!
+      @protocol = 'http'
+    end
+
+    def https!
+      @protocol = 'https'
     end
 
     def host=(host)
--- spec/lib/hubert/builder_spec.rb
+++ spec/lib/hubert/builder_spec.rb
@@ -2,18 +2,51 @@ require 'spec_helper'
 
 module Hubert
   describe Builder do
-    describe '#host=' do
+    let(:builder) do
+      Builder.new
+    end
 
-      let(:builder) do
-        Builder.new do |builder|
-          builder.host = 'http://example.com'
-        end
+    describe '#protocol=' do
+      it 'assigns the protocol' do
+        builder.protocol = 'HTTP'
+
+        expect(builder.protocol).to eq('http')
       end
 
+      it 'raises an exception if the protocol is invalid' do
+        expect { builder.protocol = 'ftp' }.
+          to raise_error(InvalidProtocol, /\[ftp\]/)
+      end
+    end
+
+    describe '#http!' do
+      it 'assigns the HTTP protocol' do
+        builder.http!
+
+        expect(builder.protocol).to eq('http')
+      end
+    end
+
+    describe '#https!' do
+      it 'assigns the HTTPS protocol' do
+        builder.https!
+
+        expect(builder.protocol).to eq('https')
+      end
+    end
+
+    describe '#host=' do
       it 'assigns the host' do
+        builder.host = 'http://example.com/'
+
         expect(builder.host).to eq('example.com')
       end
 
+      it 'removes a path from the host' do
+        builder.host = 'http://example.com/path/to/resource'
+
+        expect(builder.host).to eq('example.com')
+      end
     end
   end
 end
