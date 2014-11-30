8969a990c464c550dfa8348a72ae16abc78bfc93
Andrzej Kajetanowicz <andrzej.kajetanowicz@gmail.com>
2014-11-30 17:52:16 +0000

    Use Builder object to generate paths

--- lib/hubert/dsl.rb
+++ lib/hubert/dsl.rb
@@ -1,5 +1,20 @@
 module Hubert
   module DSL
+    class << self
+      def extended(klass)
+        builders[klass] = Builder.new
+      end
+
+      def builders
+        @builder ||= {}
+      end
+
+      def builder_for(klass)
+        builders.fetch(klass)
+      end
+    end
+
+
     def path(path, options = {})
       unless options.key?(:as)
         fail PathAliasNotSet, 'Please specify ":as" options when calling path method'
@@ -8,7 +23,7 @@ module Hubert
       method_name = "#{options[:as]}_path"
 
       define_method(method_name) do |ctx = {}|
-        Template.new(path).render(ctx)
+        DSL.builder_for(self.class).path(path, ctx)
       end
     end
   end
